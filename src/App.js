faeed33eaa5fcf260086a2b1827ee566fa5d7a03
unknown <ngovoxuantruong8@gmail.com>
2022-11-07 20:56:13 +0700

    Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            <button className="btn btn-primary">Test</button>
+        </div>
+    );
+};
+
+export default App;
